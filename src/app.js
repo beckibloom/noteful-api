2449219152fa915e9b58e485a13f34e0e7e9b7b6
Becki Blumer <rblumer13@gmail.com>
2019-09-08 10:13:07 -0500

    updates to production config

--- src/app.js
+++ src/app.js
@@ -23,10 +23,10 @@ app.use('/api/notes', notesRouter)
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
+        response = { message: error.message, error }
     } else {
         console.error(error)
-        response = { message: error.message, error}
+        response = { message: error.message, error }
     }
     res.status(500).json(response)
 })
