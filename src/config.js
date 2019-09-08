59cb22af1390c97f6dfffa06dcb60240d254d2a4
Becki Blumer <rblumer13@gmail.com>
2019-09-08 09:39:54 -0500

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful',
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-test',
+    DB_HOST: process.env.MIGRATION_DB_HOST,
+    DB_USER: process.env.MIGRATION_DB_USER,
+    DB_PASS: process.env.MIGRATION_DB_PASS,
+    DB_NAME: process.env.MIGRATION_DB_NAME
 }
